ed27078cace8b945aba193ea8e80350b0ef9a97b
purisquiri <mau.dorigo@outlook.es>
2020-11-28 17:09:16 -0500

    commit tree 960f10f7b1e4343b5f0f205bcbf3f2cf2ad55f3c

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,22 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    # render json: birds
+    render json: birds, only: [:id, :name, :species] #if we don't want update_at and create at
+    # render json: birds, except: [:created_at, :updated_at] #or this way also
+    # render json: birds.to_json(except: [:created_at, :updated_at]) #without rails magic
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    # render json: {id: bird.id, name: bird.name, species: bird.species }
+    # render json: bird.slice(:id, :name, :species) ##this work fine for a single hash
+    #render json: bird, only: [:id, :name, :species]
+    
+    if bird
+      render json: { id: bird.id, name: bird.name, species: bird.species }
+    else
+      render json: { message: 'Bird not found' }
+   end
   end
 end
